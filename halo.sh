bf2206bcfb4bb4d999786786223fafcdbf9c0e75
72ridwan <rachmat.ridwan72@gmail.com>
2019-05-23 14:47:28 +0700

    Add zerofill

--- halo.sh
+++ halo.sh
@@ -1,1 +1,6 @@
-echo $$ >> './darknet/main_pid'
+CURR_DIR="./frame/cctv_SIMPANG 4 PATUNG KUDA_patungkuda selatan 1200_x264.mp4";
+cd "./frame/cctv_SIMPANG 4 PATUNG KUDA_patungkuda selatan 1200_x264.mp4";
+hasil=`find . -maxdepth 1 -mindepth 1 -printf '%f\n'`
+echo $hasil
+# echo $$ >> './darknet/main_pid'
+# "./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg"
