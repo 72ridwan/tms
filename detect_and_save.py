cd7a557b443da596b55ebecfecd8da2d2d54d425
Rachmat Ridwan <rachmat.ridwan72@gmail.com>
2019-05-26 16:28:55 +0700

    Improve detect and save, and adding validators

--- detect_and_save.py
+++ detect_and_save.py
@@ -1,99 +1,106 @@
 #!/usr/bin/env python3
 
-# Example usage:
-# python3 detect_and_save.py -s "0000004.jpg" -e "0000005.jpg" -t "jpg" -p="../frame/cctv_SIMPANG 4 PATUNG KUDA_patungkuda selatan 1200_x264.mp4"
-
-from argparse import ArgumentParser
-import errno
 import os
 import signal
 import subprocess
-import sys
-import time
 
-parser = ArgumentParser()
-parser.add_argument("-s", "--start", dest="start", default="", action="store",
-                    help="Start frame file name, sorted alphabetically")
-parser.add_argument("-e", "--end", dest="end", action="store",  default="",
-                    help="End frame file name, sorted alphabetically")
-parser.add_argument("-t", "--file_type", action="store", dest="filetype", default="", required=True,
-                    help="File type of the image sequences. Required. Write the extension directly." +\
-                        "Example: -t=jpg")
-parser.add_argument("-p", "--folder_path", action="store", dest="folder_path", default="", required=True,
-                    help="Folder path of the images. Required.")
-                    
-args = parser.parse_args()
-start_point = args.start.strip()
-end_point = args.end.strip()
-file_type = args.filetype.strip().replace('.', '').lower()
+class Detector():
 
-is_start_point_defined = start_point != ''
-is_end_point_defined = end_point != ''
+    def __init__(self, file_type, folder_path):
+        self.file_type = file_type.replace('.', '')
+        self.folder_path = folder_path
+        self.files = []
+        self.pid = os.getpid()
+        self.flag = {'interrupted':True}
+        self.__bash_commands = ['./darknet', 'detect', 'cfg/yolov3.cfg', 'yolov3.weights', '']
 
-os.chdir("darknet")
+        if not os.path.isdir(self.folder_path):
+            raise FileNotFoundError('Folder path not found: "' + self.folder_path + '"')
+        self.files = sorted([i for i in os.listdir(self.folder_path) if os.path.splitext(i)[1] == '.' + self.file_type])
 
-global flag
-flag = {'interrupted':True}
+        with open("main_pid", "w") as outfile:
+            outfile.write(str(os.getpid()))
 
-def warn_interrupt(sig, frame):
-    flag['interrupted'] = True
+    def warn_interrupt(self, sig, frame):
+        self.flag['interrupted'] = True
 
-FOLDER_PATH = args.folder_path
-darknet_command = "./darknet"
+    def detect_single_frame(self, frame_id):
+        signal.signal(signal.SIGUSR1, self.warn_interrupt)
 
-if not os.path.isdir(FOLDER_PATH):
-    raise FileNotFoundError('Folder path not found: "' + FOLDER_PATH + '"')
+        fname = self.files[frame_id]
+        fname_name, fname_ext = os.path.splitext(fname)
 
-bashCommands = [darknet_command, 'detect', 'cfg/yolov3.cfg', 'yolov3.weights', '']
+        # Detect object locations with darknet.
+        # The output detection will be returned as CSV
+        # in hasil_prediksi.csv
 
-with open("main_pid", "w") as outfile:
-    outfile.write(str(os.getpid()))
-    print("PID of this Python process:", os.getpid())
+        self.flag['interrupted'] = False
+        self.__bash_commands[-1] = self.folder_path + '//' + fname
+        process = subprocess.Popen(self.__bash_commands, stdout=subprocess.PIPE)
 
-signal.signal(signal.SIGUSR1, warn_interrupt)
+        # Wait for the neural net process to finish...
 
-files = os.listdir(FOLDER_PATH)
+        while not self.flag['interrupted']:
+            pass
 
-i_start = 0
-i_end = len(files)-1
+        # Move the hasil_prediksi to the original image file location
+        # and rename it to match the frame number.
 
-if is_start_point_defined:
-    for i, fname in enumerate(files):
-        if fname == start_point:
-            i_start = i
-            break
-            
-if is_end_point_defined:
-    for i in range(i_start, len(files)):
-        fname = files[i]
-        if fname == end_point:
-            i_end = i
-            break
-            
-for i in range(i_start, i_end+1):
-    fname = files[i]
-    fname_name, fname_ext = os.path.splitext(fname)
-    
-    if fname_ext.replace('.', '').lower() != file_type:
-        continue
-    
-    # Detect object locations with darknet.
-    # The output detection will be returned as CSV
-    # in hasil_prediksi.csv
-    
-    flag['interrupted'] = False
-    bashCommands[-1] = FOLDER_PATH + '//' + fname
-    process = subprocess.Popen(bashCommands, stdout=subprocess.PIPE)
-    
-    # Wait for the neural net process to finish...
-    
-    while not flag['interrupted']:
-        time.sleep(0.1)
-    print("Processing {:} done!".format(fname))
-    
-    # Move the hasil_prediksi to the original image file location
-    # and rename it to match the frame number.
-    
-    boundaries_old_path = 'hasil_prediksi.csv'
-    boundaries_new_path = FOLDER_PATH + '//' + fname_name + '.csv'
-    os.rename(boundaries_old_path, boundaries_new_path)
+        boundaries_old_path = 'hasil_prediksi.csv'
+        boundaries_new_path = self.folder_path + '//' + fname_name + '.csv'
+        os.rename(boundaries_old_path, boundaries_new_path)
+        print("Processing {:} done!".format(fname))
+
+    def get_start_point_id(self, start_point):
+        i_start = 0
+        for i, fname in enumerate(self.files):
+            if fname == start_point:
+                i_start = i
+                break
+        return i_start
+
+    def get_end_point_id(self, end_point, start_index = 0):
+        i_end = len(self.files) - 1
+        for i in range(start_index, len(self.files)):
+            fname = self.files[i]
+            if fname == end_point:
+                i_end = i
+                break
+        return i_end
+
+    def detect_from_filename(self, start_point, end_point):
+        i_start = self.get_start_point_id(start_point)
+        i_end   = self.get_end_point_id(end_point, i_start)
+        print(i_start, i_end)
+
+        for i in range(i_start, i_end+1):
+            self.detect_single_frame(i)
+
+if __name__ == '__main__':
+    # Example usage:
+    # python3 detect_and_save.py -s "0000004.jpg" -e "0000005.jpg" -t "jpg" -p="../frame/cctv_SIMPANG 4 PATUNG KUDA_patungkuda selatan 1200_x264.mp4"
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--start", dest="start", default="", action="store",
+                        help="Start frame file name, sorted alphabetically")
+    parser.add_argument("-e", "--end", dest="end", action="store",  default="",
+                        help="End frame file name, sorted alphabetically")
+    parser.add_argument("-t", "--file_type", action="store", dest="filetype", default="", required=True,
+                        help="File type of the image sequences. Required. Write the extension directly." +\
+                            "Example: -t=jpg")
+    parser.add_argument("-p", "--folder_path", action="store", dest="folder_path", default="", required=True,
+                        help="Folder path of the images. Required.")
+
+    args = parser.parse_args()
+    start_point = args.start.strip()
+    end_point = args.end.strip()
+    file_type = args.filetype.strip().replace('.', '').lower()
+
+    is_start_point_defined = start_point != ''
+    is_end_point_defined = end_point != ''
+
+    os.chdir("darknet")
+    detector = Detector(file_type=file_type, folder_path = args.folder_path)
+    detector.detect_from_filename(start_point, end_point)
